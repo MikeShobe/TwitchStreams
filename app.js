fc3aa0bfa34c3516f3025889a8564629fd640ce7
MIKE SHOBE <mikeshobe@yahoo.com>
2016-03-24 16:35:57 -0700

    total results showing properly

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var xmlHttp = new XMLHttpRequest();
+var twitchData = {};
 
 xmlHttp.open('GET', encodeURI('https://api.twitch.tv/kraken/search/streams?q=starcraft'));
 xmlHttp.responseType = 'json';
@@ -7,9 +8,13 @@ xmlHttp.onload = function() {
   if (xmlHttp.status === 200) {
     console.log(JSON.stringify(xmlHttp.response));
     document.getElementById('test').innerHTML = xmlHttp.response;
+    document.getElementById('result').innerHTML = xmlHttp.response._total;
+
   }
   else {
     alert('Request failed.  Returned status of ' + xmlHttp.status);
   }
+  // console.log(twitchData);
 };
+
 xmlHttp.send();
