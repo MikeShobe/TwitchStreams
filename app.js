fb1237a2fe8375e417917a34f4e867a1058175b5
MIKE SHOBE <mikeshobe@yahoo.com>
2016-03-24 23:32:29 -0700

    added link

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ xmlHttp.responseType = 'json';
 xmlHttp.onload = function() {
   if (xmlHttp.status === 200) {
     console.log(JSON.stringify(xmlHttp.response));
-    document.getElementById('result').innerHTML = xmlHttp.response._total;
     document.getElementById('img1').src = xmlHttp.response.streams[0].preview.medium;
+    document.getElementById('link1').href = xmlHttp.response.streams[0].channel.url;
+    document.getElementById('result').innerHTML = xmlHttp.response._total;
     document.getElementById('id1').innerHTML = xmlHttp.response.streams[0].channel.display_name;
     document.getElementById('gameName1').innerHTML = xmlHttp.response.streams[0].game;
     document.getElementById('viewers1').innerHTML = '- ' + xmlHttp.response.streams[0].viewers + ' viewers';
