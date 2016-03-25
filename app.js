440a9aa620ac3ab85e27cf14538c20e0d366ae01
MIKE SHOBE <mikeshobe@yahoo.com>
2016-03-25 14:53:14 -0700

    trying something with pages & buttons

--- app.js
+++ app.js
@@ -7,14 +7,18 @@ function changeQuery(){
   xmlHttp.responseType = 'json';
 
   xmlHttp.onload = function(){
+    var results = xmlHttp.response._total;
     var pageNumber = 1;
-    var totalPages = Math.ceil(xmlHttp.response._total / 5);
+    var totalPages = Math.ceil(results / 5);
     if (xmlHttp.status === 200){
       for (var i = 0; i < 5; i++) {
-        document.getElementById('result').innerHTML = xmlHttp.response._total;
+        document.getElementById('result').innerHTML = results;
         document.getElementById('currentPage').innerHTML = pageNumber;
-        document.getElementById('totalPages').innerHTML = '/' + totalPages;        
+        document.getElementById('totalPages').innerHTML = '/' + totalPages;
+        document.getElementById('leftButton').style = 'display:inline';
+        document.getElementById('rightButton').style = 'display:inline';
         document.getElementById('img' + i).style = 'display:inline';
+        //dynamic 'stream' items
         document.getElementById('img' + i).src = xmlHttp.response.streams[i].preview.medium;
         document.getElementById('link' + i).href = xmlHttp.response.streams[i].channel.url;
         document.getElementById('id'  + i).innerHTML = xmlHttp.response.streams[i].channel.display_name;
