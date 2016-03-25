c56252fd4c1b6f8d7fd0075591a7f517b1119f3a
MIKE SHOBE <mikeshobe@yahoo.com>
2016-03-24 18:04:50 -0700

    appending api data to DOM, though hacky at moment

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var xmlHttp = new XMLHttpRequest();
-var twitchData = {};
+var query;
 
 xmlHttp.open('GET', encodeURI('https://api.twitch.tv/kraken/search/streams?q=starcraft'));
 xmlHttp.responseType = 'json';
@@ -7,14 +7,17 @@ xmlHttp.responseType = 'json';
 xmlHttp.onload = function() {
   if (xmlHttp.status === 200) {
     console.log(JSON.stringify(xmlHttp.response));
-    // document.getElementById('streamItem').innerHTML = xmlHttp.response;
     document.getElementById('result').innerHTML = xmlHttp.response._total;
+    document.getElementById('img1').src = xmlHttp.response.streams[0].preview.medium;
+    document.getElementById('id1').innerHTML = xmlHttp.response.streams[0].channel.display_name;
+    document.getElementById('gameName1').innerHTML = xmlHttp.response.streams[0].game;
+    document.getElementById('viewers1').innerHTML = '- ' + xmlHttp.response.streams[0].viewers + ' viewers';
+    document.getElementById('description1').innerHTML = xmlHttp.response.streams[0].channel.status;
 
   }
   else {
     alert('Request failed.  Returned status of ' + xmlHttp.status);
   }
-  // console.log(twitchData);
 };
 
 xmlHttp.send();
