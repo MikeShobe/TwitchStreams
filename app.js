9d1f95002d661fd81a37cabec8541f41c28df563
MIKE SHOBE <mikeshobe@yahoo.com>
2016-03-25 14:11:43 -0700

    5 streams on page, some styling

--- app.js
+++ app.js
@@ -7,14 +7,17 @@ function changeQuery(){
   xmlHttp.responseType = 'json';
 
   xmlHttp.onload = function(){
+    // var streamNumber = 0;
     if (xmlHttp.status === 200){
-      document.getElementById('img1').src = xmlHttp.response.streams[0].preview.medium;
-      document.getElementById('link1').href = xmlHttp.response.streams[0].channel.url;
-      document.getElementById('result').innerHTML = xmlHttp.response._total;
-      document.getElementById('id1').innerHTML = xmlHttp.response.streams[0].channel.display_name;
-      document.getElementById('gameName1').innerHTML = xmlHttp.response.streams[0].game;
-      document.getElementById('viewers1').innerHTML = '- ' + xmlHttp.response.streams[0].viewers + ' viewers';
-      document.getElementById('description1').innerHTML = xmlHttp.response.streams[0].channel.status;
+      for (var i = 0; i < 5; i++) {
+        document.getElementById('result').innerHTML = xmlHttp.response._total;
+        document.getElementById('img' + i).src = xmlHttp.response.streams[i].preview.medium;
+        document.getElementById('link' + i).href = xmlHttp.response.streams[i].channel.url;
+        document.getElementById('id'  + i).innerHTML = xmlHttp.response.streams[i].channel.display_name;
+        document.getElementById('gameName' + i).innerHTML = xmlHttp.response.streams[i].game;
+        document.getElementById('viewers' + i).innerHTML = '- ' + xmlHttp.response.streams[i].viewers + ' viewers';
+        document.getElementById('description' + i).innerHTML = xmlHttp.response.streams[i].channel.status;
+      }
     }
     else {
       alert('Request failed. Status code: ' + xmlHttp.status);      
